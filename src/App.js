49f96597ce875aac777b0af051b4b50aa72dc35f
keethsmith <keethsmith@ufl.edu>
2020-02-27 23:03:24 -0500

    commit\

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  componentWillMount(){
+    document.title = 'SS-KAKS'
+  }
+
+  render() { 
+    return (
+      
+        <Router>
+          
+          <Switch>
+            <Route exact path='/' component={LandingPage} />
+            {/* <Route exact path='/shop' component={Shop} />
+            <Route exact path='/admin' component={Admin} />
+            <Route exact path='/admin/shop' component={ShopAdmin} />
+            <Route exact path='/admin/testimonials' component={TestimonialsAdmin} />
+            <Route component={NotFound} /> */}
+          </Switch>
+        </Router>
+      
+    )
+  }
 }
 
 export default App;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Header from '../components/Header';
+
+
+class LandingPage extends React.Component {
+
+
+    render() {
+        return (
+            <React.Fragment style={{minHeight: window.innerHeight - 120}}>
+
+                <Header/>
+                <div style={{height: 200}}/>
+                {/* <Home/>
+                <div style={{height: 250}}/>
+                <Media/>    
+                <div style={{height: 250}}/>
+                <Testimonials/>
+                <div style={{height: 250}}/>
+                <ContactUs/>
+                <div style={{height: 175}}/>
+                <Footer/> */}
+            </React.Fragment>
+        )
+    }
+}
+  
+export default LandingPage;
