74a4c009f4ea6b096762619bd3ce8b923c94d152
Kelvin <doverkd1@gmail.com>
2020-03-18 02:43:07 -0400

    Readed LoginSystem

---
+++ loginSystem/loginSystem - Copy/frontend/src/pages/SQL.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Header from '../components/Header';
+
+
+class SQL extends React.Component {
+
+
+    render() {
+        return (
+            <React.Fragment style={{minHeight: window.innerHeight - 120}}>
+
+                <Header/>
+                <div style={{height: 200}}
+                   
+                />
+
+                    <p>yo yo ya </p>
+                
+                {/* <Home/>
+                <div style={{height: 250}}/>
+                <Media/>    
+                <div style={{height: 250}}/>
+                <Testimonials/>
+                <div style={{height: 250}}/>
+                <ContactUs/>
+                <div style={{height: 175}}/>
+                <Footer/> */}
+            </React.Fragment>
+        )
+    }
+}
+  
+export default SQL;
