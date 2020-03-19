9afc94ff9b16d91cef401bc716372f754988cbc7
Kelvin <doverkd1@gmail.com>
2020-03-19 14:42:00 -0400

    Merged Both my version and orginal branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import LandingPage from './pages/LandingPage';
 import SQL from './pages/SQL';
-import Login from "./pages/login";
-import SignUp from "./pages/signup";
+import { observer } from 'mobx-react';
+import { Provider } from "react-redux";
 import Search from "./components/search.component"
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import store from "./store";
+import Profile from './components/Profile'
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
 
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 class App extends React.Component {
   componentWillMount(){
     document.title = 'SS-KAKS'
@@ -13,16 +41,18 @@ class App extends React.Component {
 
   render() {
     return (
-
+      <Provider store={store}>
         <Router>
 
           <Switch>
             <Route exact path='/' component={LandingPage} />
-            <Route exact path ='/login' component = {Login}/>
+      
             <Route exact path='/sql' component={SQL} />
             <Route exact path='/sql/:search' component={Search}/>
-            <Route exact path ='/signup' component = {SignUp}/>
-
+            <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/profile" component={Profile} />
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
 
             {/* <Route exact path='/IDOR_URL' component={Admin} />
             <Route exact path='/admin/shop' component={ShopAdmin} />
@@ -32,10 +62,10 @@ class App extends React.Component {
           </Switch>
         </Router>
 
-
+        </Provider>
 
     )
   }
 }
 
-export default App;
+export default observer(App);
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 
 import Header from '../components/Header';
 
@@ -9,14 +9,23 @@ class LandingPage extends React.Component {
 
     render() {
         return (
-            <React.Fragment>
+            <React.Fragment style={{minHeight: window.innerHeight - 120}}>
 
                 <Header/>
-                <div style={{textAlign: "center"}}>
-                    
-                    <p>yo yo yo </p>
-
-                </div>
+                <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h4>
+              Welcome ! 
+            </h4>
+            
+            <br />
+         
+      
+          </div>
+        </div>
+      </div>>
+                
 
             </React.Fragment>
         )
