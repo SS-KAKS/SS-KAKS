c642dab97d122c88e6ec4fd6f1446e3c307de5a3
Kelvin <doverkd1@gmail.com>
2020-03-22 21:18:40 -0400

    Deployable
    Removed log out from dashboard main body. Needs PEPPER

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -31,18 +31,7 @@ return (
             
               </p>
             </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
+           
           </div>
         </div>
       </div>
