b6b35d053ac2e6f3709fcbbac4de998e12ced888
keethsmith <keethsmith@ufl.edu>
2020-03-25 11:19:27 -0400

    commit

--- src/pages/SQL.js
+++ src/pages/SQL.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import { connect } from "react-redux";
 import Header from '../components/Header';
 import Table from '../components/table.component';
-import { Link } from "react-router-dom";
 
 
 class SQL extends React.Component {
@@ -12,37 +11,7 @@ class SQL extends React.Component {
     render() {
       const { user } = this.props.auth;
       if(user.name===undefined){
-        return (
-          <React.Fragment style={{minHeight: window.innerHeight - 120}}>
-
-              <Header/>
-              <div style={{ height: "75vh" }} className="container valign-wrapper">
-      <div className="row">
-        <div className="col s12 center-align">
-          <br></br>
-          <br></br>
-          <h4>
-            You must login to access site!
-          </h4>
-          <br></br>
-            <p className="grey-text text-darken-1">
-              Already have an account? <Link to="/login">Log in</Link>
-            </p>
-            <br></br>
-            <p className="grey-text text-darken-1">
-              Don't have an account? <Link to="/register">Register</Link>
-            </p>
-
-          <br />
-
-
-        </div>
-      </div>
-    </div>>
-
-
-          </React.Fragment>
-      )
+        window.location = '/login';
 
 
       }else{
